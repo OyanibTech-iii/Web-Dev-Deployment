820333f9c04fb20cc8cbcef7a9b926b522635df5
OyanibTech-iii <pacificooyanib@gmail.com>
2026-05-26 09:56:04 +0800

    feat: added a url wss::/ secure and dynamic

--- src/Controller/Admin/ChatroomController.php
+++ src/Controller/Admin/ChatroomController.php
@@ -91,7 +91,6 @@ class ChatroomController extends AbstractController
             'admins' => $admins,
             'chatroom' => $chatroom,
             'messages' => $messages,
-            'chat_ws_url' => $this->getParameter('chat_ws_url'),
         ]);
     }
 
